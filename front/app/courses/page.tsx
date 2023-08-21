e926c7f3d945cf9edb810e6da17008688eaaff26
YoshimasaAsada <yoshimasa111112@gmail.com>
2023-08-21 17:32:48 +0900

    [add]コースの一覧ページ作成

---
+++ front/app/courses/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import type { Course } from "../../types";
+
+export default function Page() {
+  const [courses, setCourses] = useState<Course>();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data))
+      .catch((e) => console.log(e));
+  }, []);
+
+  return (
+    <>
+      <div>couses.index</div>
+      {courses?.map((course, index) => (
+        <div key={index}>
+          <Link href={`api/v1/courses/${index}`}>{course.name}</Link>
+          <br />
+        </div>
+      ))}
+    </>
+  );
+}
--- front/components/header.tsx
+++ front/components/header.tsx
@@ -17,13 +17,13 @@ export function Header() {
           ROOT
         </Link>
         <Link
-          href="/users"
+          href="/courses"
           className="font-medium px-3 py-2 text-slate-100 rounded-lg hover:bg-slate-100 hover:text-slate-900"
         >
-          ユーザー一覧
+          コース一覧
         </Link>
         <Link
-          href="#"
+          href="/courses"
           className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
         >
           sign_in
